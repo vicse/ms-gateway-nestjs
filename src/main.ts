d66440c254931d09babe4707ac61dc69331c79f2
vicse <vicseore@gmail.com>
2025-01-22 14:56:09 -0500

    feat: config envs added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(envs.port ?? 3000);
+  logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
