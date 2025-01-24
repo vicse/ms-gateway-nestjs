1d562619b82e6a3db08525e76331054dcf5147c9
vicse <vicseore@gmail.com>
2025-01-24 15:24:38 -0500

    delete: unused constants of services

--- src/common/constants/services.constant.ts
+++ src/common/constants/services.constant.ts
@@ -1,5 +1,3 @@
 export class Services {
-  static readonly PRODUCT_SERVICE = 'PRODUCT_SERVICE';
-  static readonly ORDER_SERVICE = 'ORDER_SERVICE';
   static readonly NATS_SERVICE = 'NATS_SERVICE';
 }
