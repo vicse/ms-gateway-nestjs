99c739bc0932838b67bb7327dd84633886d15e06
vicse <vicseore@gmail.com>
2025-01-28 21:15:46 -0500

    feat: exception when microservice is not responding

--- src/common/filters/rpc-custom-exception.filter.ts
+++ src/common/filters/rpc-custom-exception.filter.ts
@@ -14,6 +14,15 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
 
     const rpcError = exception.getError();
 
+    if (rpcError.toString().includes('Empty response')) {
+      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: rpcError
+          .toString()
+          .substring(0, rpcError.toString().indexOf('(') - 1),
+      });
+    }
+
     if (
       typeof rpcError === 'object' &&
       'status' in rpcError &&
