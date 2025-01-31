65cb88fe39925f97c572bea410f8014606f8c463
vicse <vicseore@gmail.com>
2025-01-31 18:20:27 -0500

    feat: verify token with auth microservice in auth guard

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,13 +1,21 @@
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { Services } from '../../common/constants';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(
+    @Inject(Services.NATS_SERVICE) private readonly client: ClientProxy,
+  ) {}
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
@@ -15,8 +23,11 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException('Token not found');
     }
     try {
-      request['user'] = { id: 1, name: 'FER', email: 'vic@,asd.com' };
-      request['token'] = token;
+      const { user, token: newToken } = await firstValueFrom(
+        this.client.send('auth.verify.token', token),
+      );
+      request['user'] = user;
+      request['token'] = newToken;
     } catch {
       throw new UnauthorizedException();
     }
